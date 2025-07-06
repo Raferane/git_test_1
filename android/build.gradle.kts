56ef2136aee80047f82412b8635e077242d858b2
Rafe <darkskies6969@gmail.com>
2025-07-06 17:20:25 +0300

    Build gradle changes

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -11,8 +11,20 @@ rootProject.layout.buildDirectory.value(newBuildDir)
 subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
+    
+    // Fix for cloud_firestore compileSdkVersion issue
+    afterEvaluate {
+        if (project.extensions.findByName("android") != null) {
+            extensions.configure<com.android.build.gradle.BaseExtension>("android") {
+                compileSdk = 34
+                defaultConfig {
+                    minSdk = 21
+                    targetSdk = 34
+                }
+            }
+        }
+    }
+    
     project.evaluationDependsOn(":app")
 }
 
